11d94f8df47ba6e403970cb0570e3a58f4ebec0e
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-17 22:33:46 +0700

    Menyesuaikan cart.php

--- cart.php
+++ cart.php
@@ -47,7 +47,7 @@ if(isset($_POST['update_qty'])){
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
   <!-- custom css file link  -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="src/css/style.css">
 
   <!-- clear confirm form resubmission -->
   <script>
@@ -184,7 +184,7 @@ if(isset($_POST['update_qty'])){
 
   <?php include 'src/components/footer.php'; ?>
 
-  <script src="js/script.js"></script>
+  <script src="src/js/script.js"></script>
 
 </body>
 
