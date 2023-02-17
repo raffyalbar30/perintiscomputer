784b55c03be5f47dda974b77f90bc541f080dcb9
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-18 04:33:44 +0700

    Up userAccount

--- admin/users_accounts.php
+++ admin/users_accounts.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../components/connect.php';
+include '../src/components/connect.php';
 
 session_start();
 
@@ -38,7 +38,7 @@ if(isset($_GET['delete'])){
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-  <link rel="stylesheet" href="../css/admin_style.css">
+  <link rel="stylesheet" href="../src/css/admin_style.css">
 
   <!-- clear confirm form resubmission -->
   <script>
@@ -90,7 +90,7 @@ if(isset($_GET['delete'])){
       style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
 
-  <?php include '../components/admin_header.php'; ?>
+  <?php include '../src/components/admin_header.php'; ?>
 
   <section class="accounts">
 
@@ -134,7 +134,7 @@ if(isset($_GET['delete'])){
 
 
 
-  <script src="../js/admin_script.js"></script>
+  <script src="../src/js/admin_script.js"></script>
 
 </body>
 
