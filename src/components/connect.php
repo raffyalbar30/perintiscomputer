fe428f565dbf2deb82d1cda1f6911722584b80e1
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-18 03:30:06 +0700

    Update

--- src/components/connect.php
+++ src/components/connect.php
@@ -4,39 +4,15 @@ $selflink = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : 
 $myPath = explode("/", $selflink);
 array_pop($myPath);
 
-$str = file_get_contents(join("/", $myPath) . "/config/config.json");
+$str = file_get_contents("conf/config.json");
 $json = json_decode($str, true);
 $dbConfig = $json['db_connect'];
 try {
-    $conn = mysqli_connect($dbConfig['host'], $dbConfig['user'], $dbConfig['pass'], $dbConfig['db_name']);
+    $conn = new PDO("mysql:host=" . $dbConfig['host'] . ";dbname=" . $dbConfig['db_name'], $dbConfig['user'], $dbConfig['pass']);
 
 } catch (PDOException $er) {
-    echo $er;
+    error_log($er, 0);
 
 }
-
-
-// Class
-class Model {
-
-    // Constructor
-    function __constructor () {
-
-
-
-    }
-
-    function getPath () {
-
-        $this->selflink = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        $this->myPath = explode("/", $selflink);
-        array_pop($this->myPath);
-        return $this->myPath;
-
-    }
-    
-}
-
-
 
 ?>
--- user_login.php
+++ user_login.php
@@ -45,7 +45,7 @@ if(isset($_POST['submit'])){
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
   <!-- custom css file link  -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="src/css/style.css">
 
   <!-- clear confirm form resubmission -->
   <script>
@@ -128,7 +128,7 @@ if(isset($_POST['submit'])){
 
   <?php include 'src/components/footer.php'; ?>
 
-  <script src="js/script.js"></script>
+  <script src="src/js/script.js"></script>
 
 </body>
 
