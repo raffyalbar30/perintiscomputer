5405a971d4cf43e2373da1ca50d36f9e63692720
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-18 00:18:35 +0700

    Up

--- src/components/connect.php
+++ src/components/connect.php
@@ -5,10 +5,11 @@ $json = json_decode($str, true);
 $dbConfig = $json['db_connect'];
 
 try {
-    $conn = mysqli_connect($dbConfig['host'], $dbConfig['user'], $dbConfig['pass'], $dbConfig['db_name']);
-} catch (\Throwable $th) {
-    //throw $th;
-    echo $th;
+    $conn = new mysqli($dbConfig['host'], $dbConfig['user'], $dbConfig['pass'], $dbConfig['db_name']);
+
+} catch (PDOException $er) {
+    echo $er;
+
 }
 
 ?>
