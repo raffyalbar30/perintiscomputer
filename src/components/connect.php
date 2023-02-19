f5c70b6ec70015a66b787d57dee381e498e7543e
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-19 16:06:01 +0700

    Up

--- src/components/connect.php
+++ src/components/connect.php
@@ -1,7 +1,7 @@
 <?php
 
-// if (!(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'))
-//     header("location:https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+if (!(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'))
+    header("location:https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
 $selflink = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $myPath = explode("/", $selflink);
 $str = "conf/config.json";
